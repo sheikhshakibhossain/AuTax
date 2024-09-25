89b83213721342162db47a7bd05a88db4c795de6
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2024-09-26 03:59:12 +0600

    emergency STOP/RESUME backend added

--- autax/ai_agent.py
+++ autax/ai_agent.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from rclpy.duration import Duration
+from std_msgs.msg import String  # Fixed import statement
 from rclpy.time import Time
 
 class CmdVelSelector(Node):
@@ -25,10 +26,17 @@ class CmdVelSelector(Node):
             self.gnss_callback,
             10
         )
+        self.emergency_sub = self.create_subscription(
+            String,
+            '/emergency',
+            self.emergency_callback,
+            10
+        )
 
         # Variables to store the latest messages from both topics
         self.latest_cmd_vel = None
         self.use_gnss = True  # Start by using GNSS as the default
+        self.emergency_stop = False
 
         # Time of the last received message
         self.last_vision_msg_time = self.get_system_time()
@@ -39,6 +47,15 @@ class CmdVelSelector(Node):
 
         # Timer to publish to /cmd_vel at regular intervals
         self.timer = self.create_timer(0.1, self.publish_cmd_vel)
+
+    def emergency_callback(self, msg):
+        msg = msg.data
+        msg = msg.strip()
+        if msg == 'STOP':
+            self.emergency_stop = True
+        elif msg == 'RESUME':
+            self.emergency_stop = False
+        self.get_logger().info(f'*** Emergency Protocol Initiated: {msg}')
 
     def get_system_time(self):
         """Helper function to return the current time using the system clock."""
@@ -65,6 +82,7 @@ class CmdVelSelector(Node):
         return end_time - start_time
 
     def publish_cmd_vel(self):
+        
         """Publishes the latest cmd_vel to /cmd_vel."""
         now = self.get_system_time()
 
@@ -76,6 +94,12 @@ class CmdVelSelector(Node):
         if self.time_difference(self.last_gnss_msg_time, now) > self.timeout_duration:
             self.get_logger().warn('No valid data from either source, stopping publication.')
             return  # Do not publish anything if no valid data
+
+        # check if user paused it from dashboard
+        if self.emergency_stop:
+            self.latest_cmd_vel.linear.x = 0.0
+            self.latest_cmd_vel.angular.z = 0.0
+            self.get_logger().info(f'*** Emergency Protocol ***')
 
         if self.latest_cmd_vel is not None:
             self.cmd_vel_pub.publish(self.latest_cmd_vel)
